7845e7a5b824ec88af3940614988aba971401add
EthanGotIt <130972892+EthanGotIt@users.noreply.github.com>
2023-06-07 18:35:17 +0800

    Update TileBoardSizeEnvironmentKey.swift

--- T2iles/Sources/Enviroment Keys/TileBoardSizeEnvironmentKey.swift
+++ T2iles/Sources/Enviroment Keys/TileBoardSizeEnvironmentKey.swift
@@ -1,6 +1,6 @@
 import SwiftUI
 
-//  定义拼图面板大小的枚举类，实现其getter和setter方法，设定默认大小为4.
+//  定义拼图面板大小的枚举类，实现其getter和setter方法，设定默认大小为4。
 struct TileBoardSizeEnvironmentKey: EnvironmentKey {
     public static let defaultValue: Int = 4
 }
