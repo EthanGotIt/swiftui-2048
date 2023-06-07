1b4d97714bbcb753e79d6291f238d26545c239f3
EthanGotIt <130972892+EthanGotIt@users.noreply.github.com>
2023-06-07 19:30:17 +0800

    Update SelectedView.swift

--- T2iles/Sources/Models/SelectedView.swift
+++ T2iles/Sources/Models/SelectedView.swift
@@ -1,13 +1,6 @@
-//
-//  SelectedView.swift
-//  T2iles
-//
-//  Created by Astemir Eleev on 31.05.2020.
-//  Copyright © 2020 Astemir Eleev. All rights reserved.
-//
-
 import Foundation
 
+//  定义枚举类SelectedView用于选择不同的视图。
 enum SelectedView {
     case game
     case settings
