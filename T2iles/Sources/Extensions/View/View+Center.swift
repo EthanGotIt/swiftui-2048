6476be090f9f9554abf57b485d1d2b8a1d4a1ea8
EthanGotIt <130972892+EthanGotIt@users.noreply.github.com>
2023-06-07 18:38:07 +0800

    Update View+Center.swift

--- T2iles/Sources/Extensions/View/View+Center.swift
+++ T2iles/Sources/Extensions/View/View+Center.swift
@@ -1,13 +1,6 @@
-//
-//  View+Center.swift
-//  T2iles
-//
-//  Created by Astemir Eleev on 18.07.2020.
-//  Copyright © 2020 Astemir Eleev. All rights reserved.
-//
-
 import SwiftUI
 
+//  定义View的扩展，实现能够使视图居中的center方法。
 extension View {
     func center(in coordinateSpace: CoordinateSpace, with proxy: GeometryProxy) -> some View {
         let frame = proxy.frame(in: coordinateSpace)
